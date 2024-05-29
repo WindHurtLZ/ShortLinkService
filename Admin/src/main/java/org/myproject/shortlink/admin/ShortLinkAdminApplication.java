b0518a352fee3102fec1b94f7bb5229eecf5d29a
Xufan Lin <xml5282@psu.edu>
2024-05-28 20:53:39 -0400

    [CREATE] configure submodules

---
+++ Admin/src/main/java/org/myproject/shortlink/admin/ShortLinkAdminApplication.java
@@ -0,0 +1,11 @@
+package org.myproject.shortlink.admin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShortLinkAdminApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ShortLinkAdminApplication.class, args);
+    }
+}
